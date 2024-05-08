cb3f1ad084bfe941a53e0b265c5f0e6f4cc765b1
Eduardo Marvil <emtv2126@gmail.com>
2024-05-08 16:41:15 -0600

    Se implementa la variable global del tiempo y se valida si es una entrada http o https

--- application/config/config.php
+++ application/config/config.php
@@ -1,6 +1,6 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-
+defined('BASEPATH') or exit('No direct script access allowed');
+date_default_timezone_set('America/Mexico_City');
 /*
 |--------------------------------------------------------------------------
 | Base Site URL
@@ -23,9 +23,18 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$root = "http://".$_SERVER['HTTP_HOST'];
-$root .= dirname($_SERVER['SCRIPT_NAME']);
-$config['base_url'] = $root;
+$config['EnableSSL'] = false;
+if (isset($_SERVER['HTTP_HOST'])) {
+    if ($config['EnableSSL'] == false) {
+        $root = "http://" . $_SERVER['HTTP_HOST'];
+    } else {
+        $root = "https://" . $_SERVER['HTTP_HOST'];
+    }
+    $root .= str_replace(basename($_SERVER['SCRIPT_NAME']), "", $_SERVER['SCRIPT_NAME']);
+} else {
+    $root = '';
+}
+$config['base_url'] = "$root";
 
 /*
 |--------------------------------------------------------------------------
@@ -54,7 +63,7 @@ $config['index_page'] = '';
 |
 | WARNING: If you set this to 'PATH_INFO', URIs will always be URL-decoded!
 */
-$config['uri_protocol']	= 'REQUEST_URI';
+$config['uri_protocol']    = 'REQUEST_URI';
 
 /*
 |--------------------------------------------------------------------------
@@ -80,7 +89,7 @@ $config['url_suffix'] = '';
 | than english.
 |
 */
-$config['language']	= 'spanish';
+$config['language']    = 'spanish';
 
 /*
 |--------------------------------------------------------------------------
@@ -410,12 +419,12 @@ $config['sess_regenerate_destroy'] = FALSE;
 |       'cookie_httponly') will also affect sessions.
 |
 */
-$config['cookie_prefix']	= '';
-$config['cookie_domain']	= '';
-$config['cookie_path']		= '/';
-$config['cookie_secure']	= FALSE;
-$config['cookie_httponly'] 	= FALSE;
-$config['cookie_samesite'] 	= 'Lax';
+$config['cookie_prefix']    = '';
+$config['cookie_domain']    = '';
+$config['cookie_path']        = '/';
+$config['cookie_secure']    = FALSE;
+$config['cookie_httponly']     = FALSE;
+$config['cookie_samesite']     = 'Lax';
 
 /*
 |--------------------------------------------------------------------------
